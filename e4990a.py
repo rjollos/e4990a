8834c196a6a1d273699531d30280f2d41071bc08
Ryan J Ollos <ryan.j.ollos@gmail.com>
2018-11-16 17:49:10 -0800

    Fix incorrect handling of return code

--- e4990a.py
+++ e4990a.py
@@ -43,14 +43,15 @@ def main(filename, config_filename):
         return 1
     inst.timeout = 15000
     try:
-        acquire(inst, config_filename)
+        rc = acquire(inst, config_filename)
     finally:
         inst.write(':SOUR:BIAS:STAT OFF')
         inst.close()
         rm.close()
 
-    input("Press [ENTER] to exit\n")
-    return 0
+    if rc == 0:
+        input("Press [ENTER] to exit\n")
+    return rc
 
 
 def acquire(inst, config_filename):
