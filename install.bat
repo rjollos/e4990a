1b16959e11c8f325dd65216f3f9c62af952fe2d2
Ryan J Ollos <ryan.j.ollos@gmail.com>
2020-12-15 14:30:18 -0800

    Make install options the same on both platforms

--- install.bat
+++ install.bat
@@ -3,8 +3,8 @@
 set venv=%~dp0venv
 C:\Python39\python.exe -m venv "%venv%"
 set python=%venv%\Scripts\python.exe
-"%python%" -m pip install -U pip
-"%python%" -m pip install -r requirements.txt
-"%python%" -m pip install -r requirements-dev.txt
+"%python%" -m pip install -U pip setuptools wheel
+"%python%" -m pip install -Ur requirements.txt
+"%python%" -m pip install -Ur requirements-dev.txt
 "%python%" -m markdown -x extra README.md > README.html
 
