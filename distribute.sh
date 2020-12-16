b72e6d3a5b20fced62e89205f54f2cd781806c6d
Ryan J Ollos <ryan.j.ollos@gmail.com>
2020-12-15 16:04:27 -0800

    Upload artifacts at end of run

--- distribute.sh
+++ distribute.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
 name=e4990a-$(date +'%Y%m%dT%H%M%S')-macos.zip
-zip -j $name \
-	dist/e4990a \
-	README.html \
-	read_e4990a_data.m \
-	plot_impedance.m
+cp README.html dist/
+cp read_e4990a_data.m dist/
+cp plot_impedance.m dist/
+zip -j $name dist/*
 echo "Created $name"
